cca038eb9fda18482dcf4428efc77c3f02e1d8c1
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2026-07-24 12:18:51 +0100

    DDCNL-12164: WIP - More urls added to IntegrationSpec

--- it/test/utils/IntegrationSpec.scala
+++ it/test/utils/IntegrationSpec.scala
@@ -17,47 +17,97 @@
 package utils
 
 import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, ok, post, urlEqualTo, urlMatching}
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, get, ok, post, urlEqualTo}
+import models.*
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.when
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.must.Matchers
+import org.scalatestplus.mockito.MockitoSugar.mock
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
 import play.api.i18n.{Lang, Messages, MessagesApi, MessagesImpl}
+import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
 import play.api.mvc.*
 import play.api.test.Injecting
+import services.CacheService.*
+import services.CachingService
 import uk.gov.hmrc.domain.{AtedUtr, Generator, Nino}
 import uk.gov.hmrc.time.TaxYear
 
-import scala.concurrent.ExecutionContext
+import java.time.LocalDate
+import scala.concurrent.{ExecutionContext, Future}
 
-trait IntegrationSpec extends PlaySpec with GuiceOneAppPerSuite with Matchers with WireMockHelper with ScalaFutures
-    with IntegrationPatience with Injecting {
+trait IntegrationSpec
+    extends PlaySpec
+    with GuiceOneAppPerSuite
+    with Matchers
+    with WireMockHelper
+    with ScalaFutures
+    with IntegrationPatience
+    with Injecting {
 
-  val generatedNino: Nino = new Generator().nextNino
-
+  val generatedNino: Nino     = new Generator().nextNino
   val generatedSaUtr: AtedUtr = new Generator().nextAtedUtr
 
-  lazy val messagesApi: MessagesApi = inject[MessagesApi]
-
+  lazy val messagesApi: MessagesApi    = inject[MessagesApi]
   implicit lazy val messages: Messages = MessagesImpl(Lang("en"), messagesApi)
 
-  val taxYear: Int = TaxYear.current.startYear
+  val taxYear: Int       = TaxYear.current.startYear
+  val instanceIdentifier = 1
+  val timeStamp          = "20130101"
+  val firstName          = "First"
+  val surname            = "Surname"
+  val date: LocalDate    = LocalDate.now()
+  val email              = "email@email.com"
+
+  val activeRecipientRelationshipRecord: RelationshipRecord = RelationshipRecord(
+    Recipient.value,
+    creationTimestamp = "20150531",
+    participant1StartDate = "20150531",
+    relationshipEndReason = Some(DesRelationshipEndReason.Default),
+    participant1EndDate = None,
+    otherParticipantInstanceIdentifier = "123456789123",
+    otherParticipantUpdateTimestamp = "20150531"
+  )
+
+  val recordList = RelationshipRecordList(Seq(activeRecipientRelationshipRecord), Some(createLoggedInUserInfo()))
+
+  val mockCachingService: CachingService = mock[CachingService]
 
   lazy val mcc: MessagesControllerComponents = inject[MessagesControllerComponents]
 
   implicit lazy val ec: ExecutionContext = app.injector.instanceOf[ExecutionContext]
 
-  override def fakeApplication(): Application = {
-    new GuiceApplicationBuilder()
-      .configure(
-        "microservice.services.auth.port" -> server.port(),
-        "microservice.services.pertax-auth.port" -> server.port()
+  def createLoggedInUserInfo(
+    name: Option[CitizenName] = Some(CitizenName(Some(firstName), Some(surname)))
+  ): LoggedInUserInfo = LoggedInUserInfo(instanceIdentifier, timeStamp, None, name)
 
+  val relationshipRecordList = RelationshipRecordList(
+    Seq(activeRecipientRelationshipRecord),
+    Some(createLoggedInUserInfo())
+  )
+
+  val maConnectorResponse = RelationshipRecordStatusWrapper(
+    relationship_record = relationshipRecordList,
+    status = ResponseStatus("OK")
+  )
+
+
+  override def fakeApplication(): Application =
+    new GuiceApplicationBuilder()
+      .overrides(
+        bind[CachingService].toInstance(mockCachingService)
+      )
+      .configure(
+        "microservice.services.auth.port"               -> server.port(),
+        "microservice.services.pertax-auth.port"        -> server.port(),
+        "microservice.services.marriage-allowance.port" -> server.port()
       )
       .build()
-  }
 
   override def beforeEach(): Unit = {
 
@@ -98,45 +148,70 @@ trait IntegrationSpec extends PlaySpec with GuiceOneAppPerSuite with Matchers wi
         .willReturn(aResponse().withBody("{\"code\": \"ACCESS_GRANTED\", \"message\": \"Access granted\"}"))
     )
 
-    val wrapperDataResponse: String =
-      """
-        |{
-        |    "menuItemConfig": [
-        |        {
-        |            "id": "home",
-        |            "text": "Check your Income Tax",
-        |            "href": "http://localhost:9230/check-income-tax/what-do-you-want-to-do",
-        |            "leftAligned": true,
-        |            "position": 0,
-        |            "icon": "hmrc-account-icon hmrc-account-icon--home"
-        |        }
-        |    ],
-        |    "ptaMinMenuConfig": {
-        |        "menuName": "Account menu",
-        |        "backName": "Back"
-        |    },
-        |    "urBanners": [
-        |        {
-        |           "page": "test-page",
-        |           "link": "test-link",
-        |           "isEnabled": true
-        |        }
-        |    ],
-        |    "webchatPages": [
-        |        {
-        |            "pattern": "^/check-income-tax/.*",
-        |            "skinElement": "HMRC_Anchored_1",
-        |            "isEnabled": true,
-        |            "chatType": "loadWebChatContainer"
-        |        }
-        |    ]
-        |}
-        |""".stripMargin
-
     server.stubFor(
-      WireMock
-        .get(urlMatching("/single-customer-account-wrapper-data/wrapper-data.*"))
-        .willReturn(ok(wrapperDataResponse))
+      get(urlEqualTo(s"/paye/$generatedNino/list-relationship"))
+        .willReturn(
+          ok.withBody(Json.toJson(maConnectorResponse).toString())
+        )
     )
+
+    val relationshipRecords                = RelationshipRecords(recordList, date)
+    val maEndingDates                      = MarriageAllowanceEndingDates(TaxYear.current.finishes, TaxYear.current.next.starts)
+    val dateOfMarriageInput                = DateOfMarriageFormInput(LocalDate.now().minusYears(2))
+    val confirmationUpdateAnswersCacheData = ConfirmationUpdateAnswersCacheData(
+      Some(relationshipRecords),
+      Some(date),
+      Some("email@email.com"),
+      Some(maEndingDates)
+    )
+
+    val recipientDetails                   = RecipientDetailsFormInput("Firstname", "Lastname", Gender("M"), Nino("AB123456A"))
+    val citizenName: CitizenName = CitizenName(Some("Test"), Some("User"))
+    val userRecord: UserRecord = UserRecord(123456789L, "2015", None, Some(citizenName))
+
+
+    when(mockCachingService.get[RelationshipRecords](eqTo(CACHE_RELATIONSHIP_RECORDS))(any()))
+      .thenReturn(Future.successful(Some(relationshipRecords)))
+
+    when(mockCachingService.get[LocalDate](eqTo(CACHE_DIVORCE_DATE))(any()))
+      .thenReturn(Future.successful(Some(date)))
+
+    when(mockCachingService.get[DateOfMarriageFormInput](eqTo(CACHE_MARRIAGE_DATE))(any()))
+      .thenReturn(Future.successful(Some(dateOfMarriageInput)))
+
+    when(mockCachingService.get[String](eqTo(CACHE_CHOOSE_YEARS))(any()))
+      .thenReturn(Future.successful(Some("2018,2019")))
+
+    when(mockCachingService.get[String](eqTo(CACHE_CHECK_CLAIM_OR_CANCEL))(any()))
+      .thenReturn(Future.successful(Some("checkMarriageAllowanceClaim")))
+
+    when(mockCachingService.get[String](eqTo(CACHE_MAKE_CHANGES_DECISION))(any()))
+      .thenReturn(Future.successful(Some("Divorce")))
+
+    when(mockCachingService.get[ConfirmationUpdateAnswersCacheData](eqTo(USER_ANSWERS_UPDATE_CONFIRMATION))(any()))
+      .thenReturn(Future.successful(Some(confirmationUpdateAnswersCacheData)))
+
+    when(mockCachingService.get[NotificationRecord](eqTo(CACHE_NOTIFICATION_RECORD))(any()))
+      .thenReturn(Future.successful(Some(NotificationRecord(EmailAddress(email)))))
+
+    when(mockCachingService.get[EmailAddress](eqTo(CACHE_EMAIL_ADDRESS))(any()))
+      .thenReturn(Future.successful(Some(EmailAddress(email))))
+
+    when(mockCachingService.get[RecipientDetailsFormInput](eqTo(CACHE_RECIPIENT_DETAILS))(any()))
+      .thenReturn(Future.successful(Some(recipientDetails)))
+
+    when(mockCachingService.put[MarriageAllowanceEndingDates](eqTo(CACHE_MA_ENDING_DATES), any())(any(), any()))
+      .thenReturn(Future.successful(maEndingDates))
+
+    when(mockCachingService.put[Boolean](eqTo(CACHE_LOCKED_CREATE), any())(any(), any()))
+      .thenReturn(Future.successful(false))
+
+    when(mockCachingService.put[UserRecord](eqTo(CACHE_TRANSFEROR_RECORD), any())(any(), any()))
+      .thenReturn(Future.successful(userRecord))
+
+    when(mockCachingService.put[RelationshipRecords](eqTo(CACHE_RELATIONSHIP_RECORDS), any())(any(), any()))
+      .thenReturn(Future.successful(relationshipRecords))
+
+    when(mockCachingService.clear()(any())).thenReturn(Future.successful(()))
   }
 }
